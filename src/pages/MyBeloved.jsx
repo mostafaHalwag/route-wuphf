158560a4988e9f6a44e8a39b9fedc9b03fd4693d
mostafaHalwag <mostafahalwag47@gmail.com>
2025-08-13 04:19:39 +0300

    Moved img to /

--- src/pages/MyBeloved.jsx
+++ src/pages/MyBeloved.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MyBeloved() {
     return (
         <div className="w-full h-screen">
-            <img src="../assets/DSC00284.JPG" alt="" className="w-full h-screen object-cover"/>
+            <img src="/DSC00284.JPG" alt="" className="w-full h-screen object-cover"/>
         </div>
     );
 }
