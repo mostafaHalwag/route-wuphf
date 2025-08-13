85bfdcb838132c2d48cb6ded575fdb0ce109383e
mostafaHalwag <mostafahalwag47@gmail.com>
2025-08-13 04:17:49 +0300

    rewritten img path

--- src/pages/MyBeloved.jsx
+++ src/pages/MyBeloved.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function MyBeloved() {
     return (
         <div className="w-full h-screen">
-            <img src="/src/assets/DSC00284.JPG" alt="" className="w-full h-screen object-cover"/>
+            <img src="../assets/DSC00284.JPG" alt="" className="w-full h-screen object-cover"/>
         </div>
     );
 }
